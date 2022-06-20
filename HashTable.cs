a7f140eb13f33fda45aed53e8cc2b89b4d402f8d
Deidara357 <deidara357357@gmail.com>
2022-06-21 02:41:10 +0300

    Add KP5

---
+++ HashTable.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace КП5
+{
+    class HashTable
+    {
+        private readonly byte _maxSize = 255;
+        private Dictionary<int, List<Item>> _items = null;
+        public IReadOnlyCollection<KeyValuePair<int, List<Item>>> Items => _items?.ToList()?.AsReadOnly();
+        public HashTable()
+        {
+            _items = new Dictionary<int, List<Item>>(_maxSize);
+        }
+        public void Insert(string key, string value)
+        {
+            if (string.IsNullOrEmpty(key))
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+
+            if (key.Length > _maxSize)
+            {
+                throw new ArgumentException($"Максимальная длинна ключа составляет {_maxSize} символов.", nameof(key));
+            }
+
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+
+            var item = new Item(key, value);
+
+            var hash = GetHashMD5(item.Key);
+            int hashKey = BitConverter.ToInt32(hash, 0);
+
+            List<Item> hashTableItem = null;
+            if (_items.ContainsKey(hashKey))
+            {
+                hashTableItem = _items[hashKey];
+
+                var oldElementWithKey = hashTableItem.SingleOrDefault(i => i.Key == item.Key);
+                if (oldElementWithKey != null)
+                {
+                    throw new ArgumentException($"Хеш-таблица уже содержит элемент с ключом {key}. Ключ должен быть уникален.", nameof(key));
+                }
+
+                _items[hashKey].Add(item);
+            }
+            else
+            {
+                hashTableItem = new List<Item> { item };
+
+                _items.Add(hashKey, hashTableItem);
+            }
+        }
+        public void Delete(string key)
+        {
+            if (string.IsNullOrEmpty(key))
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+
+            if (key.Length > _maxSize)
+            {
+                throw new ArgumentException($"Максимальная длинна ключа составляет {_maxSize} символов.", nameof(key));
+            }
+
+            var hash = GetHashMD5(key);
+            int hashKey = BitConverter.ToInt32(hash, 0);
+
+            if (!_items.ContainsKey(hashKey))
+            {
+                return;
+            }
+            
+            var hashTableItem = _items[hashKey];
+
+            var item = hashTableItem.SingleOrDefault(i => i.Key == key);
+
+            if (item != null)
+            {
+                hashTableItem.Remove(item);
+            }
+        }
+        public string Search(string key)
+        {
+            if (string.IsNullOrEmpty(key))
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+
+            if (key.Length > _maxSize)
+            {
+                throw new ArgumentException($"Максимальная длинна ключа составляет {_maxSize} символов.", nameof(key));
+            }
+
+            var hash = GetHashMD5(key);
+            int hashKey = BitConverter.ToInt32(hash, 0);
+
+            if (!_items.ContainsKey(hashKey))
+            {
+                return null;
+            }
+
+            var hashTableItem = _items[hashKey];
+
+            if (hashTableItem != null)
+            {
+                var item = hashTableItem.SingleOrDefault(i => i.Key == key);
+
+                if (item != null)
+                {
+                    return item.Value;
+                }
+            }
+
+            return null;
+        }
+        private byte[] GetHashMD5(string value)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+
+            if (value.Length > _maxSize)
+            {
+                throw new ArgumentException($"Максимальная длинна ключа составляет {_maxSize} символов.", nameof(value));
+            }
+
+            var tmpSource = ASCIIEncoding.ASCII.GetBytes(value);
+            var hash = new MD5CryptoServiceProvider().ComputeHash(tmpSource);
+            return hash;
+        }
+        private byte[] GetHashSHA256(string value)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+
+            if (value.Length > _maxSize)
+            {
+                throw new ArgumentException($"Максимальная длинна ключа составляет {_maxSize} символов.", nameof(value));
+            }
+
+            var tmpSource = ASCIIEncoding.ASCII.GetBytes(value);
+            var hash = new SHA256CryptoServiceProvider().ComputeHash(tmpSource);
+            return hash;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+
+namespace КП5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string path = @"C:\Users\user\OneDrive\Робочий стіл\1-й курс_ІК-11_Колісецький Іван\2-й семестр\Теорія алгоритмів\КП5\rating.txt";
+            string line;
+            string key;
+            var hashTable = new HashTable();
+
+            using (StreamReader sr = new StreamReader(path))
+            {
+                while ((key = sr.ReadLine()) != null)
+                {
+                    line = sr.ReadLine().TrimEnd();
+                    if (line != null) hashTable.Insert(key, line);
+                    else break;
+                }
+            }
+
+            //var timer = new Stopwatch();
+            //timer.Start();
+            //ShowHashTable(hashTable, "Хеш-функцiя MD5");
+            //timer.Stop();
+            //Console.WriteLine($"{timer.ElapsedMilliseconds} мiлiсекунд");
+
+            bool isWorking = true;
+            while (isWorking)
+            {
+                Console.WriteLine($"Щоб вивести всiх студентiв на екран натиснiть 1 \nЩоб видалити студента з таблицi та побачити оновлену таблицю натиснiть 2 \nЩоб вивести рейтинговий бал студента натиснiть 3 \nЩоб вийти, натиснiть будь-що iнше  ");
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        ShowHashTable(hashTable, "Хеш-функцiя MD5");
+                        break;
+                    case "2":
+                        Console.WriteLine($"Введіть повне iм'я студента, якого потрiбно видалити з таблицi:");
+                        line = Console.ReadLine();
+                        hashTable.Delete(line);
+                        ShowHashTable(hashTable, "Delete item from hashtable.");
+                        break;
+                    case "3":
+                        Console.WriteLine($"Введiть повне iм'я студента, якого потрiбно знайти в таблицi:");
+                        line = Console.ReadLine();
+                        var text = hashTable.Search(line);
+                        Console.WriteLine(text);
+                        break;
+                    default:
+                        isWorking = false;
+                        break;
+                }
+                Console.ReadKey();
+            }
+        }
+
+        private static void ShowHashTable(HashTable hashTable, string title)
+        {
+            if (hashTable == null)
+            {
+                throw new ArgumentNullException(nameof(hashTable));
+            }
+
+            if (string.IsNullOrEmpty(title))
+            {
+                throw new ArgumentNullException(nameof(title));
+            }
+
+            Console.WriteLine(title);
+            foreach (var item in hashTable.Items)
+            {
+                Console.WriteLine(item.Key);
+
+                foreach (var value in item.Value)
+                {
+                    Console.WriteLine($"\t{value.Key} - {value.Value}");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
